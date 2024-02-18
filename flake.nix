d828c5f0fb3645ad67812e6bc30386804118655d
diegofariasm <diegofarias016@gmail.com>
2024-02-18 21:20:43 +0000

    fix: toolchain issues when using overlay

--- flake.nix
+++ flake.nix
@@ -1,23 +1,36 @@
 {
-  description = "Description for the project";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
 
     crane = {
       url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
-    crane.inputs.nixpkgs.follows = "nixpkgs";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, crane, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-
+  outputs = inputs @ {
+    self,
+    crane,
+    flake-parts,
+    advisory-db,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
       ];
@@ -25,89 +38,140 @@
         "x86_64-linux"
       ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-
-          craneLib = crane.mkLib pkgs;
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
-
-          commonArgs = {
-            inherit src;
-          };
-
-          # The libraries.
-          # This is meant to be reused
-          # so as to not recompile it everytime.
-          cargoArtifacts = craneLib.buildDepsOnly
-            (
-              commonArgs //
-              {
-                pname = manifest.name;
-              }
-            );
-
-          # The flavours derivation.
-          # This is the package that gets consumed by nix.
-          flavoursDrv = craneLib.buildPackage
-            (
-              commonArgs //
-              {
-                inherit cargoArtifacts;
-              }
-            );
-        in
-        rec
-        {
-          # The checks for the package.
-          # This should help in development.
-          checks = {
-            inherit flavoursDrv;
-
-            # This runs clippy on the source code.
-            flavours-clippy = craneLib.cargoClippy
-              (
-                commonArgs //
-                {
-                  inherit cargoArtifacts;
-                  cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-                }
-              );
-          };
-
-          _module.args = {
-            pkgs = import inputs.nixpkgs {
-              # This makes the nixpkgs be the one
-              # suited to your current development system;
-              inherit system;
-
-              overlays = with inputs; [
-                rust-overlay.overlays.default
-              ];
-            };
-          };
-          # This generates a overlay
-          # You should consume this in your flake if you want flavours.
-          overlayAttrs = {
-            inherit (config.packages) flavours;
-          };
-
-          packages = {
-            flavours = flavoursDrv;
-          };
-          packages.default = packages.flavours;
-
-          devShells = {
-            development = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                rust-bin.stable.latest.default
-              ];
-            };
-          };
-          devShells.default = devShells.development;
-
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        toolchainPackages = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
         };
 
+        toolchain = toolchainPackages.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+          ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          pname = "flavours";
+          version = "0.0.0";
+        };
+
+        cargoArtifacts =
+          craneLib.buildDepsOnly
+          (
+            commonArgs
+            // {
+              pname = "artifacts";
+            }
+          );
+
+        flavoursDrv =
+          craneLib.buildPackage
+          (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              pname = "flavours";
+            }
+          );
+      in rec
+      {
+        checks = {
+          docs = craneLib.cargoDoc (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
+
+          fmt = craneLib.cargoFmt (commonArgs
+            // {
+              inherit src;
+            });
+
+          audit = craneLib.cargoAudit (commonArgs
+            // {
+              inherit src advisory-db;
+            });
+
+          deny = craneLib.cargoDeny (commonArgs
+            // {
+              inherit src;
+            });
+
+          clippy =
+            craneLib.cargoClippy
+            (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+              }
+            );
+
+          nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            }
+          );
+        };
+
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
+        };
+
+        overlayAttrs = {
+          inherit (config.packages) flavours;
+        };
+
+        devShells = {
+          development = craneLib.devShell {
+            checks = self.checks;
+            buildInputs = with pkgs; [
+              bacon
+              cargo-deny
+              cargo-edit
+              cargo-lock
+              cargo-nextest
+            ];
+          };
+        };
+        devShells.default = devShells.development;
+
+        packages = {
+          flavours = flavoursDrv;
+        };
+
+        formatter = pkgs.writeShellApplication {
+          name = "treefmt";
+          runtimeInputs = with pkgs; [
+            treefmt
+            rustfmt
+            alejandra
+          ];
+          text = ''
+            exec treefmt "$@"
+          '';
+        };
+      };
     };
 }
 
