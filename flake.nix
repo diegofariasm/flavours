07693e8bf5da1fd9edb6a3a76b65a1cca15f573e
diegofariasm <diegofariasm016@gmail.com>
2025-02-19 23:09:41 -0300

    bump: flake.lock; fix: flake.nix pinning wrong input for crane

--- flake.nix
+++ flake.nix
@@ -8,12 +8,7 @@
       flake = false;
     };
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    crane.url = "github:ipetkov/crane";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
